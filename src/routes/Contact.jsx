fc12768420ede1e1e2a58210a616461c359537bf
Luvous <thomasvarela2001@outlook.es>
2022-08-21 15:49:11 -0300

    Building contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -3,9 +3,21 @@ import NavBar from '../components/NavBar'
 
 function Contact() {
   return (
-    <div>
+    <section>
       <NavBar currentPath="contact" />
-    </div>
+      <div className='contact-container'>
+        <div className='contact-form'>
+          <p>Name</p>
+          Input
+          <p>Email</p>
+          Input
+          <textarea/>
+        </div>
+        <div className='<contact-info'>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
